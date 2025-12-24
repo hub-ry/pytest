832d6b774ec94cf056a5d6b16c9e6f9e119d7513
hub-ry <rhubbart@purdue.edu>
2025-12-23 23:22:06 -0500

    markings p1

--- src/my_functions.py
+++ src/my_functions.py
@@ -6,3 +6,6 @@ def divide(num1, num2):
   if num2 == 0:
     raise ValueError
   return num1 / num2
+
+
+
--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,6 +1,6 @@
 import pytest
 import src.my_functions as my_functions
-
+import time 
 
 def test_add():
     result = my_functions.add(1,4)
@@ -20,4 +20,22 @@ def test_divide_by_zero():
 
 
 
+# mark
+# pytest -m slow
+@pytest.mark.slow
+def test_very_slow():
+    time.sleep(5)
+    result = my_functions.add(1,4)
+    assert result == 5
 
+
+#skip test
+
+@pytest.mark.skip(reason="This feature is currently broken")
+def test_add():
+    assert my_functions.add(1,2) == 3
+
+# xfail, we know that this should fail
+@pytest.mark.xfail(reason="we know we cannot divide by 0")
+def test_divide_zero_broken():
+    my_functions.divide(4,0)
